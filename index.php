6c65d136052e3900c25095f19c9d88a8e914376d
Jarvis <13g7895123@gmail.com>
2022-07-09 14:07:41 +0000

    新增nav bar 分項

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <link rel="stylesheet" type="text/css" href="./assets/css/index.css">
 
-<div class='nav_bar'></div>
+<div class='nav_bar'>
+    <div class='nav_bar_main'>
+        <div class='nav_left'>
+
+        </div>
+        <div class='nav_mid'></div>
+        <div class='nav_right'></div>
+    </div>
+</div>
 
 <h1>Hello, this is bakery</h1>
 <h1>2ed changey</h1>

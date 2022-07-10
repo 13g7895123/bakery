6ecd4340499e2b24d332b0ddb6b8d5362b5be949
Jarvis <13g7895123@gmail.com>
2022-07-10 14:38:25 +0000

    新增產品及其動畫

--- index.php
+++ index.php
@@ -1,14 +1,117 @@
+<!DOCTYPE html>
+<html lang="zh-tw" >
+<head>
+    <meta charset="UTF-8">
+    <title>美濃麵包</title>
+
+    <!-- 引入JQuery -->
+    <script src="https://code.jquery.com/jquery-1.10.1.min.js"></script>
+    
+    <!-- 引入Bootstrap的CSS -->
+    <link rel="stylesheet" href="https://cdn.staticfile.org/twitter-bootstrap/5.1.1/css/bootstrap.min.css">
+
+    <!-- 引入font-awesome的CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    
+    <!-- 引入Bootstrap的js -->
+    <script src="https://cdn.staticfile.org/popper.js/2.9.3/umd/popper.min.js"></script>
+    <script src="https://cdn.staticfile.org/twitter-bootstrap/5.1.1/js/bootstrap.min.js"></script>
+
+    <!-- 引入Vue.js -->
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.26/vue.js'></script>
+
+</head>
+<body>
+
 <link rel="stylesheet" type="text/css" href="./assets/css/index.css">
 
 <div class='nav_bar'>
     <div class='nav_bar_main'>
         <div class='nav_left'>
-
+            <img src="./assets/img/logo_100.png" alt="company logo">
         </div>
         <div class='nav_mid'></div>
         <div class='nav_right'></div>
     </div>
 </div>
+<div class='content'>
+    <div class='block'>
+        <div class='bakery_img'>
+            <img src="./assets/img/bakery1_255.png" alt="company logo">
+            <div class='add_cart'>
+                <i class="fa-solid fa-cart-shopping"></i>
+                <div>加入購物車</div>
+            </div>
+        </div>
+        <div class='bakery_name'>麵包1號</div>
+        <div class='bakery_price'>$35</div>
+    </div>
+    <div class='block'>
+        <div class='bakery_img'>
+            <img src="./assets/img/bakery1_255.png" alt="company logo">
+            <div class='add_cart'>
+                <i class="fa-solid fa-cart-shopping"></i>
+                <div>加入購物車</div>
+            </div>
+        </div>
+        <div class='bakery_name'>麵包2號</div>
+        <div class='bakery_price'>$35</div>
+    </div>
+    <div class='block'>
+        <div class='bakery_img'>
+            <img src="./assets/img/bakery1_255.png" alt="company logo">
+            <div class='add_cart'>
+                <i class="fa-solid fa-cart-shopping"></i>
+                <div>加入購物車</div>
+            </div>
+        </div>
+        <div class='bakery_name'>麵包3號</div>
+        <div class='bakery_price'>$35</div>
+    </div>
+    <div class='block'>
+        <div class='bakery_img'>
+            <img src="./assets/img/bakery1_255.png" alt="company logo">
+            <div class='add_cart'>
+                <i class="fa-solid fa-cart-shopping"></i>
+                <div>加入購物車</div>
+            </div>
+        </div>
+        <div class='bakery_name'>麵包4號</div>
+        <div class='bakery_price'>$35</div>
+    </div>
+    <div class='block'>
+        <div class='bakery_img'>
+            <img src="./assets/img/bakery1_255.png" alt="company logo">
+            <div class='add_cart'>
+                <i class="fa-solid fa-cart-shopping"></i>
+                <div>加入購物車</div>
+            </div>
+        </div>
+        <div class='bakery_name'>麵包5號</div>
+        <div class='bakery_price'>$35</div>
+    </div>
+    <div class='block'>
+        <div class='bakery_img'>
+            <img src="./assets/img/bakery1_255.png" alt="company logo">
+            <div class='add_cart'>
+                <i class="fa-solid fa-cart-shopping"></i>
+                <div>加入購物車</div>
+            </div>
+        </div>
+        <div class='bakery_name'>麵包6號</div>
+        <div class='bakery_price'>$35</div>
+    </div>
+    <div class='block'>
+        <div class='bakery_img'>
+            <img src="./assets/img/bakery1_255.png" alt="company logo">
+            <div class='add_cart'>
+                <i class="fa-solid fa-cart-shopping"></i>
+                <div>加入購物車</div>
+            </div>
+        </div>
+        <div class='bakery_name'>麵包7號</div>
+        <div class='bakery_price'>$35</div>
+    </div>
+</div>
+<div class='foot_bar'></div>
 
-<h1>Hello, this is bakery</h1>
-<h1>2ed changey</h1>
